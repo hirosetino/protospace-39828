8957c95fbc2e2bfef6a714ae91c45084d1d862e8
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-01 16:28:41 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
         if comment.save
           redirect_to prototype_path(comment.prototype)
         else
-          render "prototypes/show"
+          render prototypes_path(:id)
         end
     end
 
